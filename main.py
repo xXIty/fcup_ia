4143d6cb060d3f124119f02e38c23b5435d956b6
xXIty <miquel.roset@fib.upc.edu>
2022-03-24 16:10:24 +0100

    add depth option in command line

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ def timeout(time):
 def raise_timeout(signum, frame):
     raise TimeoutError
 
-def evaluate(configInicial, configFinal, queueingFunction):
+def evaluate(configInicial, configFinal, queueingFunction, timeoutSeconds):
 
     algorithmIndex  =  list(algoritmsDict.values()).index(queueingFunction)
     algorithmKey    =  list(algoritmsDict.keys())[algorithmIndex]
 
-    with timeout(10):
+    with timeout(timeoutSeconds):
         start_time = None
 
         if queueingFunction == iterativeDepthFirstSearch:
@@ -84,7 +84,14 @@ if __name__ == "__main__":
             metavar  = 'D',
             default  = 10,
             type     = int,
-            help     = 'Maximum depth for the optimal solution.')
+            help     = 'Maximum depth for the optimal solution. Default = 10')
+
+    parser.add_argument('-t' ,
+            dest = 'timeoutSeconds',
+            metavar  = 'T',
+            default  = 10,
+            type     = int,
+            help     = 'Timeout each search after T seconds. Default = 10')
 
     parser.add_argument('-a',
             required = False,
@@ -108,24 +115,36 @@ if __name__ == "__main__":
 
     N = 4
 
+    # Final Standard Configuration of The 15 puzzle
     FSC        =  Config(N,  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0])
+
     # confA      =  Config(N,  [1,2,3,4,5,6,8,12,13,9,0,7,14,11,10,15])
     # confB      =  Config(N,  [1,2,3,4,13,6,8,12,5,9,0,7,14,11,10,15])
     # confLink1  =  Config(N,  [12,1,10,2,7,11,4,14,5,0,9,15,8,13,6,3])
     # confEasy   =  Config(N,  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,0,15])
     # confMail   =  Config(N,  [1,2,3,4,9,5,7,8,13,6,10,12,0,14,11,15])
 
+    # C1       =  (Config(N,  [1,2,3,4,9,5,7,8,13,6,10,12,0,14,11,15]),
+                 # FSC)        
+    # C2       =  (Config(N,  [9,12,13,7,0,14,5,2,6,1,4,8,10,15,3,11]),
+                 # Config(N,  [9,5,12,7,14,13,0,8,1,3,2,4,6,10,15,11]))
+    # C3       =  (Config(N,  [6,12,5,9,14,2,4,11,0,7,8,13,3,10,1,15]),
+                 # Config(N,  [14,6,12,9,7,2,5,11,8,4,13,15,3,10,1,0]))
+    # CsemSol  =  (Config(N,  [1,2,3,4,9,5,7,8,13,6,10,12,0,11,14,15]),
+                 # FSC)        
+    # configsIAMail = [C1, C2, C3, CsemSol]
+
     configsRandom  =  []
     algorithms     =  []
-    
 
+    # Generate <args.configs> random solvable configurations
     for i in range(args.configs):
         configRandom = Config(FSC.N, FSC.board)
         configRandom.scramble(args.maxOptimal)
 
         configsRandom.append(configRandom)
 
-
+    # Prepare algorithms to use from args.algoritms or use all if not specified
     if args.algorithms is None:
         algorithms = list(algoritmsDict.values())
     else:
@@ -133,39 +152,9 @@ if __name__ == "__main__":
             algorithms.append(algoritmsDict[algorithm])
 
 
-    #queueingFunction = depthFirstSearch
-    #queueingFunction = breadthFirstSearch
-    #queueingFunction = aStarSearch
-    #queueingFunction = iterativeDepthFirstSearch
-    #queueingFunction = greedySearch
-
-    #configInicial = confLink1
-    #configInicial = confEasy
-    #configInicial = confA
-    #configInicial = confMail
-
-    #configFinal   = FSC
-
-    C1       =  (Config(N,  [1,2,3,4,9,5,7,8,13,6,10,12,0,14,11,15]),
-                 FSC)        
-
-    C2       =  (Config(N,  [9,12,13,7,0,14,5,2,6,1,4,8,10,15,3,11]),
-                 Config(N,  [9,5,12,7,14,13,0,8,1,3,2,4,6,10,15,11]))
-
-    C3       =  (Config(N,  [6,12,5,9,14,2,4,11,0,7,8,13,3,10,1,15]),
-                 Config(N,  [14,6,12,9,7,2,5,11,8,4,13,15,3,10,1,0]))
-
-    CsemSol  =  (Config(N,  [1,2,3,4,9,5,7,8,13,6,10,12,0,11,14,15]),
-                 FSC)        
-
-    configsIAMail = [C1, C2, C3, CsemSol]
-    # configsRandom = [C1[0]]
-
+    # Execute each random configuration using the specified algorithms
     for config in configsRandom:
         configInicial = config
         configFinal   = FSC
         for algorithm in algorithms:
-            evaluate(configInicial, configFinal, algorithm)
-            
-
-
+            evaluate(configInicial, configFinal, algorithm, args.timeoutSeconds)
