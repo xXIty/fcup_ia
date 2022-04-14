718354e6dd4105e4453545458619e86142beabc4
xXIty <miquel.roset@fib.upc.edu>
2022-04-14 02:32:43 +0200

    Start with connect_four game bases

---
+++ t2/connect_four/src/connect_four.rs
@@ -0,0 +1,16 @@
+mod connect_four {
+
+    pub struct state {
+        // - Game board
+        // - Player turn
+    }
+
+    impl state {
+        pub  fn  is_terminal(&self)  ->  bool  {};
+
+        pub  fn  utility(&self)      ->  i32   {};
+
+        // No se com retornar una llista de state amb tamany indeterminat 
+        // pub  fn  successors(&self)   ->  
+    }
+}
---
+++ t2/connect_four/src/treenode.rs
@@ -0,0 +1,4 @@
+mod treenode {
+    struct node {
+    }
+}
