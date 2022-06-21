85a531b512365df7dda01641951e7ca3783f959b
xXIty <miquel.roset@fib.upc.edu>
2022-06-21 16:12:32 +0100

    First classes and header definitions

---
+++ t4/inc/model.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef std::vector<float> VF;
+
+class Model {
+
+    std::string name;
+    std::string trainFile;
+    std::string testFile;
+
+    private:
+        void  trainSetPreProcess();                
+        VF    evaluate(VF& input);       
+        void  train(VF& input, VF& output);
+
+    public:
+        void   setUp();
+        float  trainEpoch();
+        void   test(std::string testFile);
+
+};
